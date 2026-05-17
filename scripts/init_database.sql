3471725f769a1144e32e88232e163edaeceb8953
vamsisabbisetti <vamsisabbisetti2001@gmail.com>
2026-05-18 00:04:51 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,41 @@
+/*
+Creating database 'DataWarehouse' in sql_server
+Creating schema brone,silver,gold
+
+Warning:
+  Running the script will drop the entire 'DataWarehouse' databse if exists.
+  All data in the database will deleted permanently deleted.Proceed with caution
+  and ensure you have proper backups beforre running the scripts.
+*/
+
+USE master;
+Go
+
+--Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases where name='DataWarehouse')
+Begin
+	ALTER Database DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP Database DataWarehouse;
+END;
+GO
+
+--Create DataWarehouse database
+Create Database DataWarehouse;
+Go
+
+USE DataWarehouse;
+Go
+
+--Create Schemas
+CREATE SCHEMA bronze;
+Go
+
+CREATE SCHEMA silver;
+Go
+
+CREATE SCHEMA gold;
+Go
+
+
+
 
