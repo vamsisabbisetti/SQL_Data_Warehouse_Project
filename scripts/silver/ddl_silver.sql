1a582348b70f0d06d59a5f0a8ae3330f2c7d31c9
vamsisabbisetti <vamsisabbisetti2001@gmail.com>
2026-05-21 11:35:00 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -16,7 +16,7 @@ cst_id INT,
 cst_key NVARCHAR(50),
 cst_firstname NVARCHAR(50),
 cst_lastname NVARCHAR(50),
-cst_material_status NVARCHAR(50),
+cst_marital_status  NVARCHAR(50),
 cst_gndr NVARCHAR(50),
 cst_create_date DATE,
 dwh_create_date DATETIME2 DEFAULT GETDATE());
